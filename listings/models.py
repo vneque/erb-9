63e0ddd0dc2f954a30b846708771634b503905d3
vis.neque <vis.neque@gmail.com>
2026-05-19 12:48:47 +0800

    step 8 : search done

--- listings/models.py
+++ listings/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 from doctors.models import Doctor
+from .choices import district_choices, room_type_choices, rooms_choices
 
 class Listing(models.Model):
     doctor = models.ForeignKey(Doctor, on_delete=models.DO_NOTHING)
     title=models.CharField(max_length=200)
     address=models.CharField(max_length=200)
-    district=models.CharField(max_length=50)
+    district=models.CharField(max_length=50, choices=district_choices.items(), default="")
     choices=models.CharField(max_length=50)
     description=models.TextField(blank=True)
     services=models.CharField(max_length=200)
     service=models.IntegerField()
-    room_type=models.CharField(max_length=50)
-    rooms=models.IntegerField()
+    room_type=models.CharField(max_length=50, choices=room_type_choices.items(), default="")
+    rooms=models.IntegerField(choices=rooms_choices.items(),default=1)
     photo_main=models.ImageField(upload_to='photo/%Y/%m/%d/')
     photo_1 = models.ImageField(upload_to='photo/%Y/%m/%d/', blank=True)
     photo_2 = models.ImageField(upload_to='photo/%Y/%m/%d/', blank=True)
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from listings.models import Listing
+from listings.choices import district_choices, room_type_choices, rooms_choices 
 def index(request):
-    return render(request, "pages/index.html")
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context = {"listings": listings, "district_choices": district_choices, "room_type_choices": room_type_choices, "rooms_choices": rooms_choices}
+    return render(request, "pages/index.html", context)
 
 def about(request):
     return render(request, "pages/about.html")
